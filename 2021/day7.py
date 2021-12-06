c37710cc1292501e3db51e0a77ef1c003178d98e
Florian Ockhuysen <13467977+florianock@users.noreply.github.com>
2021-12-06 21:03:18 +0100

    Trying aocd package for downloading data day 7

--- 2021/day7.py
+++ 2021/day7.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 import os
+from aocd import data
 
 
-def main(file):
-    inputs = read_input(file)
+def main():
     print(example)
-    print(inputs)
-
-
-def read_input(filename):
-    with open(filename) as f:
-        return f.read().strip()
+    print(data)
 
 
 example = """
@@ -18,14 +13,5 @@ example = """
 """.strip()
 
 
-def get_aocd_data(day, year, file):
-    from aocd import get_data
-    with open(file, 'w') as f:
-        f.write(get_data(day=day, year=year))
-
-
 if __name__ == "__main__":
-    data_file = 'day-7-input.txt'
-    if not os.path.exists(data_file):
-        get_aocd_data(7, 2021, data_file)
-    main(data_file)
+    main()
