75476e1956f7b28597837cca6a1f6a2ce629c21f
Florian Ockhuysen <13467977+florianock@users.noreply.github.com>
2021-12-07 19:51:08 +0100

    day 7. liking the aocd package for getting data and submitting answers

--- 2021/day7.py
+++ 2021/day7.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
-import os
-from aocd import data
+
+from aocd import data, submit
 
 
 def main():
-    print(example)
-    print(data)
+    ex1 = solve(example)
+    assert ex1 == 37, f"expected 37 but got {ex1}"
+    print(solve(data))
+    ex2 = solve2(example)
+    assert ex2 == 168, f"expected 168 but got {ex2}"
+    print(solve2(data))
+
+
+def solve(inputs: str) -> int:
+    costs: list[int] = []
+    nums = [int(x) for x in inputs.split(',')]
+    for i in range(min(nums), max(nums)+1):
+        cost = [abs(x-i) for x in nums]
+        costs.append(sum(cost))
+    return min(costs)
+
+
+def solve2(inputs: str) -> int:
+    costs: list[int] = []
+    nums = [int(x) for x in inputs.split(',')]
+    for i in range(min(nums), max(nums)+1):
+        cost = [get_cost(x, i) for x in nums]
+        costs.append(sum(cost))
+    return min(costs)
+
+
+def get_cost(pos, target) -> int:
+    distance = abs(pos-target)
+    return distance * (distance + 1) // 2
 
 
 example = """
-3,4,3,1,2
+16,1,2,0,4,2,7,1,2,14
 """.strip()
 
 
