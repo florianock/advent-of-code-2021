2c08c20878cccdead7430da07ee509894ad706cb
Florian Ockhuysen <13467977+florianock@users.noreply.github.com>
2021-12-14 23:53:41 +0100

    cleaned up and made it fast

--- src/day13.py
+++ src/day13.py
@@ -2,28 +2,70 @@
 # noinspection PyUnresolvedReferences
 from aocd import data, submit
 
-# TODO clean up
+
 def main():
-    paper, instructions = read_input(data)
-    answer1 = 0
+    ex1, letter = solve(example)
+    assert ex1 == 17, f"expected 17, but got {ex1}"
+    assert letter == 'O', f"expected 'O', but got {letter}"
+    answer, letter_result = solve(data)
+    assert answer == 818, f"expected 818, but got {answer}"
+    assert letter_result == "LRGPRECB", f"expected 'LRGPRECB', but got {letter_result}"
+
+
+def solve(inputs: str):
+    paper, instructions = read_input(inputs)
+    answer = 0
     for x, i in instructions:
         paper = fold(paper, i, x)
-        if answer1 == 0:
-            answer1 = sum([sum(r) for r in paper])
-        display(paper)
-    print(answer1)
+        if answer == 0:
+            answer = sum([sum(r) for r in paper])
+    return answer, print_letters(paper)
 
-    # for answer2: squeeze eyes and read output
-    # But here goes:
-    answer2 = []
+
+def print_letters(paper: list[list[bool]]) -> str:
+    display(paper)
     font_width = 4
+    found_letters = []
     for n in range(0, len(paper[0]), font_width+1):
         key = ""
         for row in paper:
             letter = row[n:n+font_width+1]
             key += str(sum(letter))
-        answer2.append(letters[key])
-    print("".join(answer2))
+        found_letters.append(letters[key])
+    found_string = "".join(found_letters)
+    print(found_string)
+    return found_string
+
+
+def fold(paper: list[list[bool]], i: int, fold_along: str) -> list[list[bool]]:
+    if fold_along == 'y':
+        paper = transpose(paper)
+    folded = [[n or m for n, m in zip(row[:i], row[:i:-1])] for row in paper]
+    if fold_along == 'y':
+        folded = transpose(folded)
+    return folded
+
+
+def transpose(a: list[list[bool]]) -> list[list[bool]]:
+    return list(map(list, zip(*a)))
+
+
+def read_input(inputs: str) -> (list[list[bool]], list[(str, int)]):
+    [coords, folding] = inputs.split('\n\n')
+    lines = [line for line in coords.split('\n')]
+    xs, ys = ([int(line.split(',')[0]) for line in lines], [int(line.split(',')[1]) for line in lines])
+    dots = [[False for x in range(max(xs)+1)] for y in range(max(ys)+1)]
+    for x, y in zip(xs, ys):
+        dots[y][x] = True
+    folds = [(f[11], int(f[13:])) for f in folding.split('\n')]
+    return dots, folds
+
+
+def display(matrix: list[list[bool]]):
+    builder = []
+    for row in matrix:
+        builder.append("".join(["\u2588\u2588" if x else "  " for x in row]))
+    print("\n".join(builder))
 
 
 letters = {
@@ -37,54 +79,6 @@ letters = {
     "322311": 'P',
     "322322": 'R'
 }
-
-
-def fold(a: list[list[bool]], i: int, fold_along: str) -> list[list[bool]]:
-    if fold_along == 'y':
-        a = transpose(a)
-    result = []
-    for row in a:
-        left = row[:i]
-        right = row[i+1:]
-        right.reverse()
-        result.append([n or m for n, m in zip(left, right)])
-    if fold_along == 'y':
-        return transpose(result)
-    return result
-
-
-def transpose(a: list[list[bool]]) -> list[list[bool]]:
-    return list(map(list, zip(*a)))
-
-
-def read_input(inputs: str) -> (list[list[bool]], list[(str, int)]):
-    split = inputs.split('\n\n')
-    coords = split[0]
-    folding = split[1]
-    lines = [line for line in coords.split('\n')]
-    nums = []
-    for line in lines:
-        cs = line.split(',')
-        nums.append((int(cs[0]), int(cs[1])))
-    xs, ys = ([i for i, j in nums],
-              [j for i, j in nums])
-    matrix = [[True if (x, y) in nums else False for x in range(max(xs)+1)] for y in range(max(ys)+1)]
-    # for n in nums:
-    #     matrix[n[1]][n[0]] = True
-    i_lines = [i for i in folding.split('\n')]
-    result2 = [i.replace('fold along ', '') for i in i_lines]
-    result3 = []
-    for i in result2:
-        inst = i.split('=')
-        result3.append((inst[0], int(inst[1])))
-    return matrix, result3
-
-
-def display(matrix: list[list[bool]]):
-    builder = []
-    for row in matrix:
-        builder.append("".join(["##" if x else "  " for x in row]))
-    print("\n".join(builder))
 
 
 example = """
